dde0fe543f18aeabc99cd52f6116d1e1efa20561
zenglunana <2252701978@qq.com>
2022-12-14 17:57:00 +0800

    你太嗲2

--- siskin-core/src/main/scala/org/esni/siskin/core/service/Service.scala
+++ siskin-core/src/main/scala/org/esni/siskin/core/service/Service.scala
@@ -1,12 +1,12 @@
 package org.esni.siskin.core.service
 
 import grizzled.slf4j.Logging
+import org.apache.flink.streaming.api.scala.DataStream
 
-abstract class Service extends Logging {
-  def start()
+abstract class Service[IN,OUT] extends Logging {
+  def start(ds:DataStream[IN]):DataStream[OUT]
 
   def setup()
 
-  //
   def closeup()
 }
