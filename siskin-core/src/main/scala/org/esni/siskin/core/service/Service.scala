a37adfaf2fab8583433d82317e66b3884f765be1
zenglunana <2252701978@qq.com>
2022-12-15 14:12:09 +0800

    加注释

--- siskin-core/src/main/scala/org/esni/siskin/core/service/Service.scala
+++ siskin-core/src/main/scala/org/esni/siskin/core/service/Service.scala
@@ -3,7 +3,12 @@ package org.esni.siskin.core.service
 import grizzled.slf4j.Logging
 import org.apache.flink.streaming.api.scala.DataStream
 
-abstract class Service[IN,OUT] extends Logging {
+trait Service[IN,OUT] extends Logging {
+
+  //读取数据输出数据
+  //ds：输入流
+  //IN：输入流数据类型
+  //OUT：输出流数据类型
   def start(ds:DataStream[IN]):DataStream[OUT]
 
   def setup()
