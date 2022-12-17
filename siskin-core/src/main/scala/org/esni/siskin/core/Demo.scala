5227334890f83ee6467c604ec32a0b45c796ab1e
MRKKmrkk <965352611@qq.com>
2022-12-17 21:25:51 +0800

    remove conf

--- siskin-core/src/main/scala/org/esni/siskin/core/Demo.scala
+++ siskin-core/src/main/scala/org/esni/siskin/core/Demo.scala
@@ -1,10 +1,7 @@
 package org.esni.siskin.core
 
-import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
-import org.esni.siskin.core.conf.SiskinConf
+import org.apache.flink.streaming.api.scala.DataStream
 import org.esni.siskin.core.service.Service
-import org.esni.siskin.core.source.function.DataSourceFunction
-import org.esni.siskin.core.util.ReflectUtil
 
 class TestService() extends Service[String, String]{
   override def start(ds: DataStream[String]): DataStream[String] = null
@@ -16,22 +13,7 @@ object Demo {
 
   def main(args: Array[String]): Unit = {
 
-    println(SiskinConf.TEST)
 
-    val ts = ReflectUtil.reflect[TestService]("org.esni.siskin.core.TestService")
-    ts.setup()
-
-    val service = ReflectUtil.reflectAndReload[Service[String, String]]("org.example.AutoService")
-    service.setup()
-
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
-    val testDataService = new DataSourceService()
-
-
-    testDataService.setup()
-    val value = testDataService.start(env)
-    value.print()
-    env.execute()
 
   }
 
